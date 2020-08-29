00870e2e267c9f48f1a3f3cf69421ff4b1bf8c2f
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2020-08-28 21:49:16 -0400

    Made search bar functional Switched to class based view for the homescreen due to higher complexity

--- custom_components/homescreen.js
+++ custom_components/homescreen.js
@@ -1,28 +1,72 @@
-import React from 'react';
-import { Text } from '@ui-kitten/components';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { Text, Input } from '@ui-kitten/components';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { Cardtest } from './card'
 
 let cardlist = []
 for (i = 0; i < 10; i++) {
-  title = 'Header ' + String(i)
-  text = ';akdf;lsajflasjkflsjflsjflsjflskjf;asldkfjnidsffoahfa;jf;ajf'
-  cardlist.push(<Cardtest title={title} text={text} />)
+  titles = 'Header ' + String(i)
+  texts = ';akdf;lsajflasjkflsjflsjflsjflskjf;asldkfjnidsffoahfa;jf;ajf'
+  cardlist.push({title: titles, text: texts})
 }
 
-export const HomeScreen = () => (
-  <ScrollView
-    contentInsetAdjustmentBehavior="automatic"
-    style={styles.scrollView}>
-    <View style={styles.body}>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Find Recipies</Text>
-        {cardlist}
+export class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: cardlist,
+      error: null,
+    };
+
+    this.datacopy = cardlist;
+  }
+
+  searchFilterFunction = text => {
+    copy = [...this.datacopy]
+    if (text != '') {
+      var newData = copy.filter(item => {
+        return item.title.includes(text)
+      });
+    } else {
+      var newData = copy
+    }
+
+    this.setState({
+      data: newData,
+    });
+  };
+
+  render() {
+    return (
+      <View>
+        <View style={styles.body}>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>Find Recipies</Text>
+            <Input 
+              onChangeText={text => this.searchFilterFunction(text)} 
+              status='basic' 
+              placeholder='Search by keyword' />
+            <View style={{height: '80%'}}>
+              <FlatList
+                contentInsetAdjustmentBehavior="automatic"
+                showsVerticalScrollIndicator={false}
+                style={styles.scrollView}
+                data={this.state.data}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
+                  <Cardtest title={item.title} text={item.text} />
+                )}>
+              </FlatList>
+            </View>
+          </View>
+        </View>
       </View>
-    </View>
-  </ScrollView>
-);
+    )
+  }
+};
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -31,14 +75,17 @@ const styles = StyleSheet.create({
   },
   body: {
     backgroundColor: Colors.white,
+    height: '100%'
   },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
+    height: '100%'
   },
   sectionTitle: {
     fontSize: 34,
     fontWeight: '600',
     color: Colors.black,
+    marginBottom: 15,
   },
 })
