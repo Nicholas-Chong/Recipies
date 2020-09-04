e0e7fbf5fd909296a4dfd539c8df67968ad218bb
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2020-09-03 22:47:24 -0400

    Create pass params func function passes the entire object to the next view

--- custom_components/homescreen.js
+++ custom_components/homescreen.js
@@ -6,21 +6,22 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { Cardtest } from './card'
 import { SelectQuery } from './database'
 
-let cardlist = []
-for (i = 0; i < 10; i++) {
-  titles = 'Header ' + String(i)
-  texts = ';akdf;lsajflasjkflsjflsjflsjflskjf;asldkfjnidsffoahfa;jf;ajf'
-  cardlist.push({title: titles, text: texts, id: String(i)})
-}
+// let cardlist = []
+// for (i = 0; i < 10; i++) {
+//   titles = 'Header ' + String(i)
+//   texts = 'this is a test description for some tupe fo delicius food afkljaslfjsl'
+//   cardlist.push({title: titles, text: texts, id: String(i)})
+// }
+
+// let test = new ArrayBuffer(cardlist)
+// console.log(test)
 
 export class HomeScreen extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      loading: false,
       data: [],
-      error: null,
       datacopy: []
     };
   }
@@ -40,45 +41,41 @@ export class HomeScreen extends Component {
     });
   };
 
-  onCardClick = () => {
-    this.props.navigation.navigate('Details', {foo: 'alsfjlsjfslfjs'})
+  onCardClick = (item) => {
+    this.props.navigation.navigate('Details', {itemData: item})
   };
 
   async componentDidMount() {
     await SelectQuery().then(dt => this.setState({data: dt, datacopy: dt}))
   }
 
-  render() {
-    console.log(this.state.data, 'RENDER')
-    
-    if (this.state.loading == false) {
-      return (
-        <View style={styles.body}>
-          <View style={styles.sectionContainer}>
-            <Text style={styles.sectionTitle}>Find Recipies</Text>
-            <Input 
-              onChangeText={text => this.searchFilterFunction(text)} 
-              status='basic' 
-              placeholder='Search by keyword'
-              style={{marginBottom: 15}} />
-            <View style={{flex: 1}}>
-              <FlatList
-                contentInsetAdjustmentBehavior="automatic"
-                showsVerticalScrollIndicator={false}
-                style={styles.scrollView}
-                data={this.state.data}
-                keyExtractor={(item) => item.id}
-                renderItem={({item}) => (
-                  <TouchableOpacity onPress={this.onCardClick} >
-                    <Cardtest title={item.title} text={item.description} />
-                  </TouchableOpacity>
-                )}>
-              </FlatList>
-            </View>
+  render() {    
+    return (
+      <View style={styles.body}>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Find Recipies</Text>
+          <Input 
+            onChangeText={text => this.searchFilterFunction(text)} 
+            status='basic' 
+            placeholder='Search by keyword'
+            style={{marginBottom: 15}} />
+          <View style={{flex: 1}}>
+            <FlatList
+              contentInsetAdjustmentBehavior="automatic"
+              showsVerticalScrollIndicator={false}
+              style={styles.scrollView}
+              data={this.state.data}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({item}) => (
+                <TouchableOpacity key={item.id} onPress={() => {this.onCardClick(item)}} >
+                  <Cardtest title={item.title} text={item.description} />
+                </TouchableOpacity>
+              )}>
+            </FlatList>
           </View>
         </View>
-      )
-    }
+      </View>
+    )
   }
 };
 
